1ec735317e40fe99706809ee9500afdaf8a63c63
Eugene Fedorenko <eugene.fedorenko@eproseed.com>
2016-09-01 12:53:45 +0200

    Assignments model

--- adf/coe/EproseedCoeCommonApp/EproseedCoeCommonModel/src/main/java/com/eproseed/coe/common/model/adfbc/base/CoeEntityImpl.java
+++ adf/coe/EproseedCoeCommonApp/EproseedCoeCommonModel/src/main/java/com/eproseed/coe/common/model/adfbc/base/CoeEntityImpl.java
@@ -1,9 +1,40 @@
 package com.eproseed.coe.common.model.adfbc.base;
 
+import oracle.jbo.AttributeList;
 import oracle.jbo.server.EntityImpl;
+import oracle.jbo.server.SequenceImpl;
 
 public class CoeEntityImpl extends EntityImpl {
     public CoeEntityImpl() {
         super();
     }
+    
+    protected void initializePrimaryKeyValue() {
+        
+    }
+
+    @Override
+    public void create(AttributeList attributeList) {
+      super.create(attributeList);
+      initializePrimaryKeyValue();
+    }
+      
+    
+    protected void initializeDefaultValues() {
+        
+    }
+
+
+    @Override
+    protected void initDefaultExpressionAttributes(AttributeList nameValuePair) { 
+      super.initDefaultExpressionAttributes(nameValuePair);
+      initializeDefaultValues();
+    }
+    
+    protected Long getNextSequenceValue(String sequenceName) {
+      SequenceImpl seq = new SequenceImpl(sequenceName, getDBTransaction());
+      return seq.getSequenceNumber().longValue();
+    }
+
+
 }
